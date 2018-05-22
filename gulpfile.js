b7a04dac6c8601873ef575c6c5745d5d2632cac4
Tony <tmedinger@gmail.com>
2018-05-22 11:14:54 -0400

    Setup update from gitbook

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,17 @@
-/*
-Gulp will be used for 
-developent and deployoment tasks,
-such a minification of code,
-translating SCSS into SASS & CSS,
-and reloading after a save.
-
-See the README.md for gulp commands.
-
+/* Gulp will be used for developent and deployoment tasks,
+such a minification of code, translating SCSS into SASS & CSS,
+and reloading after a save. See the README.md for gulp commands.
 */
+var gulp        = require("gulp");
+var browserSync = require("browser-sync").create();
+var reload      = browserSync.reload;
+
+gulp.task("serve", function() {
+    // Serve files from the root of the project
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        }
+    });
+    gulp.watch("*.html").on("change", reload);
+})
